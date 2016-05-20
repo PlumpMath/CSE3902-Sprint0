a1189c9e60b46234b616bbdbbc291530c8d303be
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-05-20 18:45:18 -0400

    Quite the game if the Q key is pressed

--- CSE3902-Sprint0/KeyboardController.cs
+++ CSE3902-Sprint0/KeyboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,19 @@ namespace CSE3902_Sprint0
 {
     class KeyboardController : IController
     {
+        public Game1 Game { get; set; }
+
         public KeyboardController(Game1 game)
         {
+            Game = game;
 
+        }
+
+        public void Update()
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.Q)) {
+                Game.Exit();
+            }
         }
     }
 }
