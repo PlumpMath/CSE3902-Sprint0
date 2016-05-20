5ec639e1953e9147d2d2cc507de18dd26a6a3f9f
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-05-20 11:52:48 -0400

    Create interfaces for IController and ISprite

---
+++ CSE3902-Sprint0/IController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE3902_Sprint0
+{
+    interface IController
+    {
+    }
+}
