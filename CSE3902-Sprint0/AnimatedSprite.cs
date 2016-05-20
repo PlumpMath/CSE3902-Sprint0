4970ef9d38f6b7eaf611bd5ee820f201d0c60579
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-05-20 15:08:16 -0400

    Initialize AnimatedSprite, which implements ISprite interface

---
+++ CSE3902-Sprint0/AnimatedSprite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CSE3902_Sprint0
+{
+    class AnimatedSprite : ISprite
+    {
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+        public Texture2D Texture
+        {
+            get; set;
+        }
+
+        public void Draw(SpriteBatch spritebatch, Vector2 location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
