d049aeb82626d7c8e1ef8afa81a4fb64da05e71d
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-05-21 21:57:34 -0400

    Accept the source rectangle when initializing a static sprite

--- CSE3902-Sprint0/Sprites/StaticSprite.cs
+++ CSE3902-Sprint0/Sprites/StaticSprite.cs
@@ -13,24 +13,30 @@ namespace CSE3902_Sprint0
         public int Columns { get; set; }
         public int Rows { get; set; }
         public Texture2D Texture { get; set; }
+        public Rectangle SourceRectangle { get; set; }
 
-        public StaticSprite(Texture2D texture)
+        public StaticSprite(Texture2D texture, Rectangle sourceRectangle)
         {
             Columns = 1;
             Rows = 1;
 
             Texture = texture;
-
+            SourceRectangle = sourceRectangle;
         }
 
-        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        public void Draw(SpriteBatch spriteBatch, Vector2 destination)
         {
-            throw new NotImplementedException();
+            Rectangle destinationRectangle = new Rectangle((int)destination.X,
+                                                           (int)destination.Y,
+                                                           SourceRectangle.Width,
+                                                           SourceRectangle.Height);
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, SourceRectangle, Color.White);
+            spriteBatch.End();
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
         }
     }
 }
